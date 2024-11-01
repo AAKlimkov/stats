0edaf19e3c6f3555fdd462854509b1632c34a2be
Andrey Klimkov <aaklimkov@gmail.com>
2024-11-01 16:43:31 +0300

    init base project

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+  },
+};
+
+export default nextConfig;
